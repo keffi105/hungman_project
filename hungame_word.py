a99f099b4a26700c695f8d9753282abbcc7ca740
keffi105 <munauwarahkeffi@gmail.com>
2024-07-09 00:18:03 +0100

    hungman word

--- hungame_word.py
+++ hungame_word.py
@@ -1,4 +1,4 @@
 word_list = ['camel','book','cable','ruler','table','kaduna','engineering','zaria','beautiful',
              'phone','clock','hydrogen','electrical','mechanical','computer','nigeria',
              'textbook','hydrometer','voltmeter','ammeter','mutimeter','classes','identity','ibadan','kano',
-             'nasarawa','delter','arewa','college','bubble']
+             'nasarawa','delter','arewa','college','bubble','instant']
